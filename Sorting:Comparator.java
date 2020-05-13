f4d14710cc8b793f6ac07bfe00c042b9a9256a02
mittal88812 <60611645+mittal88812@users.noreply.github.com>
2020-05-13 10:05:54 +0530

    Create Sorting:Comparator.java

---
+++ Sorting:Comparator.java
@@ -0,0 +1,16 @@
+class Checker implements Comparator<Player> {
+  	// complete this method
+	public int compare(Player a, Player b) {
+
+        if(a.score<b.score){
+            return(1);
+        }
+        else if(a.score>b.score){
+           return(-1);
+        }
+        else{
+            return((a.name).compareTo(b.name));
+        }
+    
+    }
+}
